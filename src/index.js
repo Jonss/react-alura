ecabde3d88249c8c98c0e49678220ce50e0ccf71
Joao Santana <joaosantana.ti@gmail.com>
2017-11-19 21:37:19 -0200

    aula 6 - usando react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import AutorBox from './Autor';
+import Home from './Home';
+import {BrowserRouter as Router,Route, Switch} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(
+	(<Router>
+		<App>
+			<Switch>
+				<Route exact path="/" component={Home}/>
+				<Route path="/autor" component={AutorBox}/>
+				<Route path="/livro"/>
+			</Switch>
+		</App>
+	</Router>)
+	, document.getElementById('root'));
