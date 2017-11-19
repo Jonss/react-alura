ef7ff3fc35f92425e146d02ebe5a7fec709ccdd4
Joao Santana <joaosantana.ti@gmail.com>
2017-11-19 20:09:54 -0200

    aula 5 - criando components e isolando do app

--- src/App.js
+++ src/App.js
@@ -4,124 +4,42 @@ import './css/pure.css'
 import './css/side-menu.css'
 import $ from 'jquery'
 import CustomInput from './components/CustomInput'
+import AutorBox from './Autor'
+
 
 class App extends Component {
-
-  constructor() {
-    super();
-    this.state = { lista: [], nome:'', email:'', senha: ''};
-    this.enviaForm = this.enviaForm.bind(this);
-    this.setNome = this.setNome.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setSenha = this.setSenha.bind(this);
-
-  }
-
-  componentDidMount() {
-    $.ajax({
-      url: "http://localhost:8080/api/autores",
-      dataType: "json",
-      success: function(resposta){
-        this.setState({lista:resposta});
-      }.bind(this)
-    });
-  }
-
-
-  enviaForm(e) {
-    e.preventDefault();
-    $.ajax({
-      url: "http://localhost:8080/api/autores",
-      contentType: "application/json",
-      dataType: "json",
-      type: 'post',
-      data: JSON.stringify({nome:this.state.nome, email:this.state.email, senha:this.state.senha}),
-      success: function(resposta) {
-          this.setState({lista:resposta});
-      }.bind(this),
-      error: function(resposta) {
-          console.log("Deu erro")
-      }
-
-    });
-  }
-
-  setNome(evento){
-    this.setState({nome:evento.target.value});
-  }
-
-  setEmail(evento) {
-    this.setState({email:evento.target.value});
-  }
-
-  setSenha(evento) {
-    this.setState({senha:evento.target.value});
-  }
 
   render() {
     return (
       <div id="layout">
 
-    <a href="#menu" id="menuLink" className="menu-link">
+        <a href="#menu" id="menuLink" className="menu-link">
+            <span></span>
+        </a>
 
-        <span></span>
-    </a>
+        <div id="menu">
+            <div className="pure-menu">
+                <a className="pure-menu-heading" href="#">CDC</a>
 
-    <div id="menu">
-        <div className="pure-menu">
-            <a className="pure-menu-heading" href="#">CDC</a>
-
-            <ul className="pure-menu-list">
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
+                <ul className="pure-menu-list">
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
 
 
-            </ul>
+                </ul>
+            </div>
         </div>
-    </div>
 
         <div id="main">
-            <div className="header">
-              <h1>Cadastro de Autores</h1>
-            </div>
-            <div className="content" id="content">
-              <div className="pure-form pure-form-aligned">
-                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm}>
-
-                  <CustomInput type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"/> 
-                  <CustomInput type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email"/> 
-                  <CustomInput type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"/>                  
-
-
-
-                  <CustomInput type="submit" value="Gravar" className="pure-button pure-button-primary"/>
-                </form>             
-
-              </div>  
-              <div>            
-                <table className="pure-table">
-                  <thead>
-                    <tr>
-                      <th>Nome</th>
-                      <th>email</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {this.state.lista.map(function(autor) {
-                      return (
-                        <tr> 
-                          <td>{autor.nome}</td>
-                          <td>{autor.email}</td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table> 
-              </div>             
-            </div>
+          <div className="header">
+            <h1>Cadastro de Autores</h1>
+          </div>
+          <div className="content" id="content">
+               <AutorBox/> 
           </div>            
-      </div> 
+        </div> 
+      </div>
     );
   }
 }
